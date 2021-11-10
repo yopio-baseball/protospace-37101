c7a13c625981b6c213e5be5522ada2919a66e787
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-10 12:19:02 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,70 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:edit,:destroy]
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit]
+
+  def index
+    @prototypes = Prototype.all.includes(:user)
+   
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path  
+    else
+      render :new
+    end 
+  end
+
+  def show
+    
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+   
+  end
+
+  def update
+    
+    if @prototype.update(prototype_params)
+      redirect_to prototypes_path
+    else
+      render :edit
+      
+    end
+  end
+
+  def destroy
+    
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  
+
+  private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    
+    unless current_user.id == @prototype.user_id 
+      redirect_to action: :index
+    end
+  end
+
+
+end
